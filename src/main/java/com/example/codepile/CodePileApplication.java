0d69d9288534d3ef21f2530867f8839c8dd90589
Йордан Йорданов <dani_yordani@abv.bg>
2021-08-11 12:20:21 +0300

    Initial create

---
+++ src/main/java/com/example/codepile/CodePileApplication.java
@@ -0,0 +1,13 @@
+package com.example.codepile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodePileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodePileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/codepile/CodePileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codepile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodePileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
