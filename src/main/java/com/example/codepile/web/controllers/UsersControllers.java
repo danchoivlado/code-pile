f6df844aeed309a93a261f10d7283ecac7a35a3f
Йордан Йорданов <dani_yordani@abv.bg>
2021-08-12 14:43:23 +0300

    Index and login done

--- src/main/java/com/example/codepile/web/controllers/UsersControllers.java
+++ src/main/java/com/example/codepile/web/controllers/UsersControllers.java
@@ -3,14 +3,16 @@ package com.example.codepile.web.controllers;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class HomeController extends BaseController {
+@RequestMapping("/users")
+public class UsersControllers extends BaseController {
 
-    @GetMapping({"/", "/home"})
-    @PreAuthorize("permitAll()")
-    public ModelAndView index(){
-        return super.view("index");
+    @GetMapping("login")
+    @PreAuthorize("isAnonymous()")
+    public ModelAndView login(){
+        return view("/users/login");
     }
 }
